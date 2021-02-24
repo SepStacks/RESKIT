53cdaf99906d2abb36e499bf7bd37cc9e009e128
Jermaine Sepkitt <jermaine.sepkitt@altron.com>
2021-02-24 11:47:02 +0200

    removed content plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,6 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -62,8 +61,7 @@ export default {
     }
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
+
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
